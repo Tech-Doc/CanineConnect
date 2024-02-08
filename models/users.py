4e87cb5c7002a705fe60b5efdb917325fd9ed2b5
Tech-Doc <dr.stanleyperpetual@gmail.com>
2024-02-09 00:05:35 +0100

    Update users.py

--- models/users.py
+++ models/users.py
@@ -3,7 +3,6 @@ from datetime import datetime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import relationship
 
-
 class Userz(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
